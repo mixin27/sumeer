acc4e88964d626ed0a9add7d89a9dc13c495d3d5
Kyaw Zayar Tun <kyawzayartun.dev@gmail.com>
2023-07-24 09:47:52 +0630

    firebase: configure firebase app

--- lib/app_init.dart
+++ lib/app_init.dart
@@ -3,18 +3,33 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter_displaymode/flutter_displaymode.dart';
 
-import 'package:sumeer/utils/utils.dart';
+import 'package:sumeer/shared/shared.dart';
 
 class AppInit {
   static Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
 
-    PlatformDispatcher.instance.onError = (error, stack) {
-      eLog(error.toString(), [error, stack]);
-      return true;
-    };
+    /// Firebase initialization
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    // FirebaseCrashlytics
+    if (kReleaseMode) {
+      // Pass all uncaught "fatal" errors from the framework to Crashlytics
+      FlutterError.onError =
+          FirebaseCrashlytics.instance.recordFlutterFatalError;
+
+      // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+      PlatformDispatcher.instance.onError = (error, stack) {
+        FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+        return true;
+      };
+    }
 
     /// https://github.com/flutter/flutter/issues/35162
     if (Platform.isAndroid) {
--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,6 +1,9 @@
+export 'config/firebase/firebase_options.dart';
 export 'config/routes/app_router.dart';
 export 'config/routes/app_router.gr.dart';
 export 'config/routes/app_router_observer.dart';
 
 export 'constants/app_strings.dart';
 export 'constants/asset_paths.dart';
+
+export 'providers/firebase_providers.dart';
