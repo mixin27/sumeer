98d9d194e8304f030c439faf99f5638fdf9b0d02
Myat Soe <myatsoe.sbs@gmail.com>
2023-07-26 15:29:44 +0630

    changed personDetailroute to detailRoute

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -190,7 +190,7 @@ class _HomePageState extends State<HomePage> {
         ),
         ElevatedButton.icon(
           onPressed: () {
-            context.router.push(const PersonalDetailRoute());
+            context.router.push(const DetailRoute());
           },
           style: FilledButton.styleFrom(
             shape: RoundedRectangleBorder(
