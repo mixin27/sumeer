526262bcba4a8231f171011efeac30ca2d206365
theintmyatnoe.sbs <theintmyatnoe.sbs@gmail.com>
2023-08-04 15:58:36 +0630

    cv template new'

--- lib/features/resume/core/presentation/templates/resume_templates.dart
+++ lib/features/resume/core/presentation/templates/resume_templates.dart
@@ -104,4 +104,11 @@ const resumeTemplates = <ResumeTemplate>[
     generateTemplate9,
     type: DocumentType.resume,
   ),
+  ResumeTemplate(
+    'CV Template 1',
+    'cv_template_1.dart',
+    AssetPaths.cvTemplate1,
+    generateCVTemplate1,
+    type: DocumentType.cv,
+  ),
 ];
